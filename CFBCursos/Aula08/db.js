86ca2204b9953f311032ba31428f87416cf9aed3
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2025-01-20 16:56:43 -0300

    CRUD com Node e MySQL
    Node com banco P2-A e P3-A do CFB Cursos

--- CFBCursos/Aula08/db.js
+++ CFBCursos/Aula08/db.js
@@ -12,4 +12,22 @@ const todosCliente = async () => {
     const [linhas] = await con.query('SELECT * FROM cliente_node')
     return await linhas
 }
-module.exports = { todosCliente }
+const insereCliente = async (cliente) => {
+    const con = await conectar()
+    const sql = 'INSERT INTO cliente_node (nome,idade) VALUES (?,?)'
+    const valores = [cliente.nome, cliente.idade]
+    await con.query(sql, valores)
+}
+const atualizaCliente = async (id, cliente) => {
+    const con = await conectar()
+    const sql = 'UPDATE cliente_node SET nome=?, idade=? WHERE id=?;'
+    const valores = [cliente.nome, cliente.idade, id]
+    await con.query(sql, valores)
+}
+const deletarCliente = async (id) => {
+    const con = await conectar()
+    const sql = 'DELETE FROM cliente_node WHERE id=?;'
+    const valores = [id]
+    await con.query(sql, valores)
+}
+module.exports = { todosCliente, insereCliente, atualizaCliente, deletarCliente }
--- CFBCursos/Aula08/index.js
+++ CFBCursos/Aula08/index.js
@@ -1,6 +1,20 @@
 (async () => {
     const db = require('./db')
-    console.log('Selecionar todos os clientes')
+    // console.log('novo cliente inserido')
+    // const nom = "Brastorgilsan"
+    // const ida = 20
+    // await db.insereCliente({ nome: nom, idade: ida })
+
+    // const id = 2
+    // const nom = "Lesvirson"
+    // const ida = 38
+    // await db.atualizaCliente(id, { nome: nom, idade: ida })
+    // console.log('cliente ' + id + ' atualizado')
+
+    const id = 2
+    await db.deletarCliente(id)
+    console.log('cliente ' + id + ' deletado')
+    console.log('Obter todos os clientes')
     const clientes = await db.todosCliente()
     console.log(clientes)
 })()
